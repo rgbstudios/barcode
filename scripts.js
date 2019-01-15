cc484d63370942895d0026e2572b0354516ee653
Justin Golden <justingolden21@gmail.com>
2019-01-15 14:20:19 -0800

    testing service worker

--- scripts.js
+++ scripts.js
@@ -1,3 +1,23 @@
+// Register the ServiceWorker limiting its action to those URL starting
+// by `controlled`. The scope is not a path but a prefix. First, it is
+// converted into an absolute URL, then used to determine if a page is
+// controlled by testing it is a prefix of the request URL.
+navigator.serviceWorker.register('service-worker.js', {
+  scope: './controlled'
+});
+
+// Load controlled and uncontrolled pages once the worker is active.
+navigator.serviceWorker.ready.then(reload);
+
+// Reload both iframes.
+function reload() {
+	location.reload();
+}
+
+
+
+
+
 //https://github.com/lindell/JsBarcode
 $(function() {
 	let url = new URL(window.location.href);
